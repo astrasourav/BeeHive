e80fce4c4c1b25b403475ed124e3dca304de1762
Sourav Kumar <50782204+astrasourav@users.noreply.github.com>
2021-06-26 01:14:08 +0530

    Completed the Backend but it does not contain any database rignt now

--- app.js
+++ app.js
@@ -3,33 +3,56 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const app = express();
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+let posts = [];
 
-app.get("/", function(req, res) {
-    res.render("home");
+app.get("/", function (req, res) {
+  res.render("home", {
+    posts: posts,
+  });
 });
 
-app.get("/compose", function(req, res){
-    res.render("compose");
+app.get("/posts/:postID", function (req, res) {
+  const value = _.lowerCase(req.params.postID);
+
+  posts.forEach(function (post) {
+    const title = _.lowerCase(post.title);
+
+    if (value === title) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
-app.post("/compose", function(req, res) {
-    console.log(req.body.blogTitle);
-    console.log(req.body.blogBody)
+app.get("/compose", function (req, res) {
+  res.render("compose");
 });
 
-app.get("/contact", function(req, res) {
-    res.render("contact");
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.blogTitle,
+    content: req.body.blogBody,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
-app.get("/about", function(req, res) {
-    res.render("about");
+app.get("/contact", function (req, res) {
+  res.render("contact");
 });
 
-app.listen(3000, function() {
-    console.log("server started at port 3000");
-})
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function () {
+  console.log("server started at port 3000");
+});
